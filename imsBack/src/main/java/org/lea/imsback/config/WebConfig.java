0df8113a162cdde8e6d7c504f7193e8fc6530414
EstebanLescano <estebanles22@gmail.com>
2025-11-03 15:54:57 -0300

    corrijo para que funcione swagger

---
+++ imsBack/src/main/java/org/lea/imsback/config/WebConfig.java
@@ -0,0 +1,20 @@
+package org.lea.imsback.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class WebConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Aplica a todas las rutas del Back-End
+                .allowedOrigins("http://localhost:9092") // Permite al Gateway
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
